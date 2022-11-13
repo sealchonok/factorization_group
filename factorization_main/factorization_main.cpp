4388a99290d25f6ed4b7e95befb446f1574496a1
PakhomCh <pakhomch@gmail.com>
2022-11-13 20:21:47 +0300

    Changed test values

--- factorization_main/factorization_main.cpp
+++ factorization_main/factorization_main.cpp
@@ -8,30 +8,37 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <locale>
+
 #include "Remizova.h"
 #include "Romanov.h"
 #include "Stoler.h"
+#include "Pakhomov.h"
+
 using namespace std;
+
 void print(std::vector<int> const& input)
 {
     for (int i = 0; i < input.size(); i++) {
-        std::cout << input.at(i) << ' ';
+        cout << input.at(i) << ' ';
     }
 }
 
 template <typename T>
 void Run(string method_name, Method<T> p, vector<int> data)
 {
-    cout << method_name << endl;
+    cout << endl << method_name << endl;
     for (int i = 0; i < data.size(); i++) {
+        cout << "Число: " << data[i] << endl;
+
         auto start = chrono::system_clock::now();
         vector<T> res = p(data[i]);
         auto stop = chrono::system_clock::now();
         auto time = chrono::duration_cast<chrono::microseconds>(stop - start).count();
 
-        cout << data[i] << ":\n";
+        cout << "\tРазложение: ";
         print(res);
-        cout << "\n" << time << " ms.\n\n";
+        cout << endl << "\tВремя (мкс): " << time << endl;
     }
 
 
@@ -40,13 +47,16 @@ void Run(string method_name, Method<T> p, vector<int> data)
 
 int main()
 {
-    vector<int> data = { 300564, 319, 18419, 123455, 23449, 84923, 2225, 300, 35, 150, 12, 30, 444, 22, 660, };
+    setlocale(LC_ALL, "Ru");
+
+    vector<int> data = { 13029, 202429, 591157, 891265, 1113121, 25060027, 33550369, 59549297, 102941291, 396501869, 1244502576, 1403943942 };
    
     
-   //Run("Pollard Po", PollardPo, data);
-   //Run("Pollard_P1", Pollard_P1, data);
-   Run("Prime Factorization", primeFactorization, data);
-   Run("Dixon Factorization", DixonFactor, data);
+    // Run("Pollard Po", PollardPo, data);
+    Run("Pollard_P1", Pollard_P1, data);
+    Run("Prime Factorization", primeFactorization, data);
+    // Run("Dixon Factorization", DixonFactor, data);
+    Run("Lenstra’s Elliptic Curve Factorization", EllipticCurveFactorisation, data);
   
 }
 

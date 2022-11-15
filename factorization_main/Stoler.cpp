e2fa6ba42e08ebcef2bfef99f4ed12694e65f206
StolerDanielAA2107 <danielstoler@yandex.ru>
2022-11-15 10:39:02 +0300

    Исправлен Поллард ро

--- factorization_main/Stoler.cpp
+++ factorization_main/Stoler.cpp
@@ -4,19 +4,29 @@
 #include <numeric>
 using namespace std;
 
+int singlePollard(int n) {
+	vector<int> val;
+	int temp;
+	val.push_back(1);
+
+	for (int i = 1; i <= n; i++) {
+
+		temp = val.back();
+		val.push_back((val.back() * val.back() + 1) % n);
+		int d = gcd(n, abs(val[i] - val[i / 2]));
+		if (d > 1)
+			return d;
+	}
+}
+
 vector<int> PollardPo(int n) {
-    vector<int> ans;
-    for (int x = 1; x < n - 2; ++x) {
-        int y = 1; int i = 0; int stage = 2;
-        while (gcd(abs(x - y), n) == 1) {
-            if (i == stage) {
-                y = x;
-                stage = stage * 2;
-            }
-            x = (x * x + 1);
-            i += 1;
-        }
-        ans.push_back(gcd(abs(x - y), n));
-    }
-    return ans;
+	vector<int> ans;
+
+	while (n != 1)
+	{
+		ans.push_back(singlePollard(n));
+		n /= ans.back();
+	}
+
+	return ans;
 }
